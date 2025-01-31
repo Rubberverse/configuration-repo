4ab2d8d510ba89700c8c965943b7e980154b0d9c
Mr. Rubber Ducky <79613254+MrRubberDucky@users.noreply.github.com>
2025-01-31 03:48:11 +0100

    Create run.sh
    Signed-off-by: Mr. Rubber Ducky <79613254+MrRubberDucky@users.noreply.github.com>

---
+++ Homarr/run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ "$DB_MIGRATIONS_DISABLED" == "true" ]; then
+  echo "DB migrations are disabled, skipping"
+else
+    echo "Running DB migrations"
+    node ./db/migrations/"$DB_DIALECT"/migrate.cjs ./db/migrations/"$DB_DIALECT"
+fi
+
+AUTH_SECRET=$(openssl rand -base64 32)
+export AUTH_SECRET
+
+redis-server /app/redis.conf &
+
+node apps/tasks/tasks.cjs &
+
+node apps/websocket/wssServer.cjs &
+
+node apps/nextjs/server.js &
+
+exec tail -f /dev/null
